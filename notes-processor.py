369be41e094b970ef274534bd3ef295d290ee028
Henry de Valence <git@hdevalence.ca>
2012-11-14 17:40:26 -0500

    Don't just guess which vertices are which.

--- notes-processor.py
+++ notes-processor.py
@@ -44,17 +44,34 @@ def findSquares(image):
 	#cv2.imwrite("tmp__b.png", cdraw)
 	return squares
 
+
 def warpSheet(image):
+	h, w = image.shape
 	squares = findSquares(image)
 	sheet = max(squares,key=lambda s: cv2.arcLength(s,True))
-	sourcePoints = sheet[::,0,::].astype('float32')
-	r,c = image.shape
-	destPoints = np.array([[c,0],
-	                       [0,0],
-	                       [0,r],
-	                       [c,r]],dtype='float32')
-	transform = cv2.getPerspectiveTransform(sourcePoints,destPoints)
-	return cv2.warpPerspective(image,transform,(c,r))
+	#We don't know how the points of the square we found are ordered. 
+	#So we just study it out, and we'll see.
+	src = sheet[::,0,::].astype('float32')
+	# Compute distances from topleft corner (0,0)
+	# to find topleft and bottomright
+	d = np.sum(np.abs(src)**2,axis=-1)**0.5
+	t_l = np.argmin(d)
+	b_r = np.argmax(d)
+	# Compute distances from topright corner (w,0)
+	# to find topright and bottomleft
+	y = np.array([[w,0],]*4)
+	d = np.sum(np.abs(src-y)**2,axis=-1)**0.5
+	t_r = np.argmin(d)
+	b_l = np.argmax(d)
+	#Now assemble these together
+	destH, destW = h, int(h*8.5/11.0)
+	dest = np.zeros(src.shape,dtype='float32')
+	dest[t_l] = np.array([0,0])
+	dest[t_r] = np.array([destW,0])
+	dest[b_l] = np.array([0,destH])
+	dest[b_r] = np.array([destW,destH])
+	transform = cv2.getPerspectiveTransform(src,dest)
+	return cv2.warpPerspective(image,transform,(destW,destH))
 
 if __name__ == "__main__":
 	map(processImage,sys.argv[1:])
